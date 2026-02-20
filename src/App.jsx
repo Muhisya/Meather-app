edc2975348ed3c2deb8eef342e2cfd6481e52f11
Muhammad Hibban Syakir <144977923+Muhisya@users.noreply.github.com>
2026-02-20 09:26:13 +0700

    up

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="min-h-screen bg-[#0B0E14] selection:bg-blue-500/30 overflow-x-hidden text-white">
       <Navbar onSearch={setCity} onLogout={handleLogout} user={user} />
-      <main className="relative">
+      <main className="relative pb-5 pt-2">
         <div className="fixed top-[-10%] left-[-10%] w-[60%] h-[60%] bg-blue-900/10 blur-[130px] rounded-full pointer-events-none opacity-50"></div>
         <div className="fixed bottom-[-10%] right-[-10%] w-[50%] h-[50%] bg-indigo-900/10 blur-[130px] rounded-full pointer-events-none opacity-50"></div>
         
